8d9038e7785d30f8a2bcfcd5ab3f860348597c94
Tinesh <tineshshah87@gmail.com>
2017-05-29 17:58:33 +0200

    tab2tab3

---
+++ app/src/main/java/com/example/tinesh/t_tabbed/Tab2.java
@@ -0,0 +1,19 @@
+package com.example.tinesh.t_tabbed;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Tinesh on 5/29/2017.
+ */
+
+public class Tab2 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab2,container,false);
+    }
+}
--- app/src/main/java/com/example/tinesh/t_tabbed/TabTin.java
+++ app/src/main/java/com/example/tinesh/t_tabbed/TabTin.java
@@ -137,7 +137,19 @@ public class TabTin extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+         switch(position)
+         {case 0:
+             Tab1 tab1=new Tab1();
+             return tab1;
+             case 1:
+             Tab2 tab2=new Tab2();
+             return tab2;
+             case 2:
+             Tab3 tab3=new Tab3();
+             return tab3;
+
+         }
+         return null;
         }
 
         @Override
